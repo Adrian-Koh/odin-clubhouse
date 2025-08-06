2281c4666beb032ffc55064b90ec723d816d108b
Adrian-Koh <adriankoh216@yahoo.com>
2025-08-07 00:37:19 +0800

    Populate index page with dummy messages
    User is only shown when isMember is true

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -5,8 +5,12 @@ links.push({ href: "/login", title: "Log In" });
 links.push({ href: "/join-member", title: "Be a member" });
 links.push({ href: "/new-message", title: "New message" });
 
+const dummyMessages = [];
+dummyMessages.push({ user: "user1", text: "hello" });
+dummyMessages.push({ user: "user2", text: "sup" });
+
 function getHomepage(req, res) {
-  res.render("index", { links });
+  res.render("index", { links, messages: dummyMessages, isMember: false });
 }
 
 module.exports = { getHomepage };
