f3f5906107f6729b83030515a5b6fca9f0c3a569
Adrian-Koh <adriankoh216@yahoo.com>
2025-08-06 18:31:28 +0800

    Set up connect-pg-simple
    Had issue with 'session_pkey already exists' - deleting cookie fixed it

--- app.js
+++ app.js
@@ -1,17 +1,42 @@
 const path = require("node:path");
-const session = require("express-session");
 require("dotenv").config();
 const express = require("express");
+const pg = require("pg");
+const expressSession = require("express-session");
+const pgSession = require("connect-pg-simple")(expressSession);
 const app = express();
-const indexRouter = require("./routes/indexRouter");
+//const indexRouter = require("./routes/indexRouter");
 
-app.use(express.static(path.join(__dirname, "public")));
-app.use("/", indexRouter);
-
-app.use((err, req, res, next) => {
-  res.status(500);
-  res.render("error", { error: err });
+const pgPool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
 });
+console.log(process.env.DATABASE_URL);
+
+app.use(
+  expressSession({
+    store: new pgSession({
+      pool: pgPool,
+      tableName: "clubhouse_sessions",
+      createTableIfMissing: true,
+    }),
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 }, // 30 days
+  })
+);
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+//app.use("/", indexRouter);
+app.get("/", (req, res) => res.send("welcome!"));
+
+// app.use((err, req, res, next) => {
+//   res.status(500);
+//   res.send(err);
+//   //res.render("error", { error: err });
+// });
 
 const PORT = 8000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}.`));
